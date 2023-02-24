516aad8767b95a58caf494b087db262f08c2b122
NicolauSouza21 <algumacoisan@gmail.com>
2023-02-24 18:03:52 -0200

    24/02

---
+++ ExercicioOperadores/NotaAluno.java
@@ -0,0 +1,12 @@
+package ExercicioOperadores;
+
+public class NotaAluno {
+    public void Operadores(){
+        System.out.println("Se a nota do aluno for maior que ");
+        int primeiraVariavel = 1;
+        int segundaVariavel = 1;
+        boolean primeiraVariavelEMaiorQueASegunda = primeiraVariavel > segundaVariavel;
+        
+    }
+    
+}
---
+++ Operadores/App.java
@@ -0,0 +1,16 @@
+package Operadores;
+
+public class App {
+public static void main(String[] args){ 
+    Operadores obj1 = new Operadores();
+    obj1.aritimetico();
+Operadores obj2 = new Operadores();
+obj2.atribuicao();
+obj1.relacionais();
+}
+
+}
+
+  
+   
+
